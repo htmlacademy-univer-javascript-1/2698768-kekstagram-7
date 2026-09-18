f62f5a790e67fa1adcdfac94939bebb38292c3eb
Alisa Pirogova <pirogovaalisa457@gmail.com>
2026-09-18 20:58:13 +0500

    выполнил module2-task1

---
+++ js/functions.js
@@ -0,0 +1,30 @@
+//1 task
+export function checkLengthString(str, maxLen)  {
+  return (str.length <= maxLen);
+}
+
+
+//2 task
+export function isPalindrom(str) {
+  const normalized = str.replaceAll(' ', '').toLowerCase();
+  let reversed = '';
+
+  for (let i = normalized.length - 1; i >=0; i--) {
+    reversed += normalized[i];
+  }
+  return normalized === reversed;
+}
+
+
+//3 task
+export function makeNumber(str) {
+  let digits = '';
+
+  for(let i = 0; i < str.length; i++){
+    if (!Number.isNaN(parseInt(str[i]))){
+      digits+= str[i];
+    }
+  }
+  return parseInt(digits);
+}
+
